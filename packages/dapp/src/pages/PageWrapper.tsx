835967667e58c809e5f19fbadf905e0c9b1bdbee
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-02-23 00:11:43 +0200

    feat(org.id-profile-manager): PageWrapper commponent now supports breadcrumbs config

--- packages/dapp/src/pages/PageWrapper.tsx
+++ packages/dapp/src/pages/PageWrapper.tsx
@@ -1,16 +1,23 @@
 import type { ReactNode } from 'react';
+import type { Breadcrumb } from '../components/Breadcrumbs';
 import { useContext } from 'react';
 import { Box, ResponsiveContext } from 'grommet';
+import { Breadcrumbs } from '../components/Breadcrumbs';
 
 export interface PageWrapperProps {
-  children: ReactNode
+  children: ReactNode;
+  breadcrumbs?: Breadcrumb[];
 }
 
-export const PageWrapper = ({ children }: PageWrapperProps) => {
+export const PageWrapper = ({ children, breadcrumbs }: PageWrapperProps) => {
   const size = useContext(ResponsiveContext);
 
   return (
-    <Box pad={size}>
+    <Box fill pad={size}>
+      <Breadcrumbs
+        breadcrumbs={breadcrumbs}
+        size={size}
+      />
       {children}
     </Box>
   );
