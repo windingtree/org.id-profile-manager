1f572ae4db1a9384ce9364c9cfbcb0392f35e02a
Kostiantyn Smyrnov <kostysh@gmail.com>
2022-02-22 23:47:14 +0200

    refactor(org.id-profile-manager): added responsive option to App component. AppHeader moved inside the Main wrapper

--- packages/dapp/src/App.tsx
+++ packages/dapp/src/App.tsx
@@ -1,18 +1,19 @@
-import { useContext } from 'react';
-import { Main, ResponsiveContext } from 'grommet';
+import { Main } from 'grommet';
 import { AppStateProvider } from './store';
 import { GlobalStyle } from './GlobalStyle';
 import { AppHeader } from './components/AppHeader';
 import { AppRoutes } from './components/Routes';
 
 const App = () => {
-  const size = useContext(ResponsiveContext);
 
   return (
     <AppStateProvider>
       <GlobalStyle>
-        <AppHeader />
-        <Main pad={size}>
+        <Main
+          fill
+          responsive
+        >
+          <AppHeader />
           <AppRoutes />
         </Main>
       </GlobalStyle>
